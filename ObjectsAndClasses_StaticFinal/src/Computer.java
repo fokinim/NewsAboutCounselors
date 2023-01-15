9d2fad99e00259b3bc2f0c896902fcb8f332a109
Igor Fokin <i.fokin33@mail.ru>
2023-01-15 18:36:39 +0300

    Модуль 6 - задание 2 - доработанное - 2

--- ObjectsAndClasses_StaticFinal/src/Computer.java
+++ ObjectsAndClasses_StaticFinal/src/Computer.java
@@ -65,34 +65,6 @@ public class Computer {
         this.keyboard = keyboard;
     }
 
-/*    public Computer setProcessor(int frequency, int coresAmount,
-                                 ProcessorManufacturer processorManufacturer,
-                                 int processorWeight) {
-        processor = new Processor(frequency, coresAmount, processorManufacturer, processorWeight);
-        return processor;
-    }
-
-    public Computer setRam(int ramAmount, RamType ramType, int ramWeight) {
-        ram = new Ram(ramAmount, ramType, ramWeight);
-        return new Computer(vendor, name, processor, ram, hardDrive, display, keyboard);
-    }
-
-    public Computer setHardDrive(int hardDriveVolume, int hardDriveWeight, HardDriveType hardDriveType) {
-        hardDrive = new HardDrive(hardDriveVolume, hardDriveWeight, hardDriveType);
-        return new Computer(vendor, name, processor, ram, hardDrive, display, keyboard);
-    }
-
-    public Computer setDisplay(int displayDiagonal, int displayWeight, DisplayType displayType) {
-        display = new Display(displayDiagonal, displayWeight, displayType);
-        return new Computer(vendor, name, processor, ram, hardDrive, display, keyboard);
-    }
-
-    public Computer setKeyboard(boolean keyboardLight, int keyboardWeight, KeyboardType keyboardType) {
-        keyboard = new Keyboard(keyboardLight, keyboardWeight, keyboardType);
-        return new Computer(vendor, name, processor, ram, hardDrive, display, keyboard);
-    }
-    */
-
     public String toString() {
         message = "Производитель: " + vendor + "\n" + "Имя: " + name + "\n" + processor.toString() + "\n" +
                 ram.toString() + "\n" + hardDrive.toString() + "\n" + display.toString() + "\n" +
