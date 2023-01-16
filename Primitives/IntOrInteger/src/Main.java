fe01ad1b5d85d26b18e5dce962227f42c644d51d
Igor Fokin <i.fokin33@mail.ru>
2023-01-16 16:07:47 +0300

    Модуль 7 - задания

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -8,5 +8,26 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+        char startSymbol = 'А';
+        int startSymbolCode = startSymbol;
+
+        char finishSymbol = 'я';
+        int finishSymbolCode = finishSymbol;
+
+        char yoSymbol = 'Ё';
+        int yoSymbolCode = yoSymbol;
+
+        char smallYoSymbol = 'ё';
+        int smallYoSymbolCode = smallYoSymbol;
+
+
+        for (int i = startSymbolCode; i <= finishSymbolCode; i++) {
+            System.out.println(i + " - " + (char) i);
+            if ((char) i == 'Е') {
+                System.out.println(yoSymbolCode + " - " + yoSymbol);
+            } else if ((char) i == 'е'){
+                System.out.println(smallYoSymbolCode + " - " + smallYoSymbol);
+            }
+        }
     }
 }
