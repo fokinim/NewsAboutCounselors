aca14f770f750113b5e8699ddda332a1aaba5d36
Igor Fokin <i.fokin33@mail.ru>
2023-02-12 19:06:07 +0300

    Практическая работа 13.14

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,41 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+import com.skillbox.airport.Terminal;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
+
+    public static final int MILLS_IN_HOURS = 7_200_000;
     public static void main(String[] args) {
 
+        System.out.println(findPlanesLeavingInTheNextTwoHours(Airport.getInstance()));
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+
+        ArrayList<Terminal> terminal = new ArrayList<>(airport.getTerminals());
+
+        ArrayList<Flight> flights = new ArrayList<>();
+
+        List<Flight> foundFlights;
+
+        terminal.forEach(t -> flights.addAll(t.getFlights()));
+        foundFlights = flights.stream().filter(Main::planesLeavingInTheNextTwoHoursFilter).collect(Collectors.toList());
+
+        return foundFlights;
+    }
+
+    public static boolean planesLeavingInTheNextTwoHoursFilter(Flight flight) {
+
+        Date currentDate = new Date();
+
+        long timeDifference = (flight.getDate().getTime() - currentDate.getTime());
+
+        return timeDifference <= MILLS_IN_HOURS && timeDifference > 0 && flight.getType() == Flight.Type.DEPARTURE;
     }
 
 }
